c6deaaa3412adfc8f96687f7577eb07a370b36ec
Gareth <blackfireweb@gmail.com>
2024-09-12 20:58:54 +0100

    Add in automatic game location and mod detection

---
+++ MistriaLocator.cs
@@ -0,0 +1,34 @@
+﻿namespace Garethp.ModsOfMistriaInstaller;
+
+public class MistriaLocator
+{
+    public static string? GetMistriaLocation()
+    {
+        var steamLocations = GetSteamLocations();
+        var mistriaLocation = steamLocations
+            .Select(location => Path.Combine(location, "common", "Fields of Mistria"))
+            .FirstOrDefault(location => Directory.Exists(location) && File.Exists(Path.Combine(location, "data.win")));
+
+        return mistriaLocation;
+    }
+    
+    private static IEnumerable<string> GetSteamLocations()
+    {
+        var locations = new List<string>
+        {
+            @"C:\Program Files (x86)\Steam\steamapps\",
+            @"C:\Program Files\Steam\steamapps\",
+            $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}/steam/steam/steamapps/"
+        };
+
+        locations.AddRange(
+            DriveInfo
+                .GetDrives()
+                .Select(drive => $@"{drive.RootDirectory.Name}/SteamLibrary/steamapps/")
+        );
+
+        return locations
+            .Where(Path.Exists)
+            .ToList();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,16 +3,45 @@
 using System.Diagnostics;
 using Garethp.ModsOfMistriaInstaller;
 
+var modOverride = "D:\\SteamLibrary\\steamapps\\common\\RimWorld\\Mods\\FoMInstaller\\mods";
+
+var detectedLocation = MistriaLocator.GetMistriaLocation();
+if (detectedLocation == null)
+{
+    Console.WriteLine("Could not find Fields of Mistria location.");
+    return;
+}
+
+var detectedModsLocation = modOverride;
+
+if (modOverride is null || !Directory.Exists(modOverride))
+{
+    detectedModsLocation =
+        Path.Combine(detectedLocation, "mods");
+}
+
+if (!Directory.Exists(detectedModsLocation))
+{
+    detectedModsLocation = Path.Combine(detectedLocation, "Mods");
+}
+
+if (!Directory.Exists(detectedModsLocation))
+{
+    Console.WriteLine($"Could not find a mods folder at {Path.Combine(detectedLocation, "mods")}.");
+    return;
+}
+
+Console.WriteLine($"Guessed Location: {detectedLocation}");
+
 var totalTime = new Stopwatch();
 totalTime.Start();
 
-var modsFolder = "D:\\SteamLibrary\\steamapps\\common\\RimWorld\\Mods\\FoMInstaller\\mods";
 var mods = Directory
-    .GetDirectories(modsFolder)
+    .GetDirectories(detectedModsLocation)
     .Where(folder => File.Exists(Path.Combine(folder, "manifest.json")))
     .ToList();
 
-var installer = new ModInstaller("D:\\SteamLibrary\\steamapps\\common\\Fields of Mistria");
+var installer = new ModInstaller(detectedLocation);
 
 installer.InstallMods(mods);
 
