1ecb14405ca5465c5be79dba382967f55fc1eddf
Gareth <blackfireweb@gmail.com>
2025-03-09 16:38:27 +0000

    Add a quick test to make sure that the UI is functional

---
+++ ModsOfMistriaGUITests/TestAppBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia;
+using Avalonia.Headless;
+using Garethp.ModsOfMistriaGUI;
+using Projektanker.Icons.Avalonia;
+using Projektanker.Icons.Avalonia.FontAwesome;
+
+namespace ModsOfMistriaGUITests;
+
+// [assembly: AvaloniaTestApplication(typeof(TestAppBuilder))]
+public class TestAppBuilder
+{
+    public static AppBuilder BuildAvaloniaApp()
+    {
+        IconProvider.Current.Register<FontAwesomeIconProvider>();
+
+        return AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .UseHeadless(new AvaloniaHeadlessPlatformOptions());
+    }
+}
