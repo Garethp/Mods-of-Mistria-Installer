68139e32d3d304d123c77ac0d8dd4e6a5886aecd
Gareth Parker <blackfireweb@gmail.com>
2025-03-09 17:06:19 +0000

    Try setting up in a different way

--- ModsOfMistriaGUITests/MainWindowTest.cs
+++ ModsOfMistriaGUITests/MainWindowTest.cs
@@ -31,7 +31,7 @@ public class Tests
                 .UsePlatformDetect()
                 .WithInterFont()
                 .LogToTrace()
-                .SetupWithClassicDesktopLifetime([]);
+                .SetupWithoutStarting();
         });
     }
 }
