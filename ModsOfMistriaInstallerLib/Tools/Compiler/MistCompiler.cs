bed9a0fe3383ea5817dab4bac34a2dca4be499d6
Gareth <blackfireweb@gmail.com>
2025-08-25 23:11:40 +0100

    Start work on the mist decompiler

---
+++ ModsOfMistriaInstallerLib/Tools/Compiler/MistCompiler.cs
@@ -0,0 +1,21 @@
+﻿using Esprima;
+
+namespace Garethp.ModsOfMistriaInstallerLib.Tools.Compiler;
+
+public class MistCompiler
+{
+    public string Compile(string input)
+    {
+        if (!File.Exists(input))
+        {
+            throw new Exception("File not found: " + input);
+        }
+
+        var contents = File.ReadAllText(input);
+
+        var parser = new JavaScriptParser();
+        var script = parser.ParseScript(contents);
+        
+        return "";
+    }
+}
