100e6554c1df11c7f1486b92d2285d2c08c58cfe
jumpyapple <70656949+jumpyapple@users.noreply.github.com>
2025-09-01 00:54:43 -0400

    Add basic structures for decompilation of Mist AST

--- ModsOfMistriaInstallerLib/Tools/Decompiler/MistDecompiler.cs
+++ ModsOfMistriaInstallerLib/Tools/Decompiler/MistDecompiler.cs
@@ -1,8 +1,200 @@
 ﻿using Esprima;
 using Esprima.Ast;
+using Esprima.Utils;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Garethp.ModsOfMistriaInstallerLib.Tools.Decompiler;
+
+public class MistContainer
+{
+    public List<MistProgram> Programs;
+}
+
+public class MistProgram
+{
+    public string Name;
+    public NodeList<Node> Statements;
+
+    public string ToJavaScriptString()
+    {
+        string result = "";
+        foreach (var stmt in Statements)
+        {
+            if (stmt != null)
+            {
+                result += AstToJavaScript.ToJavaScriptString(stmt);
+            }
+        }
+        return result;
+    }
+}
+
+public class MistContainerConverter : JsonConverter<MistContainer>
+{
+    public override MistContainer? ReadJson(JsonReader reader, Type objectType, MistContainer? existingValue, bool hasExistingValue, JsonSerializer serializer)
+    {
+        JToken token = JToken.Load(reader);
+
+        if (token.Type == JTokenType.Object)
+        {
+            Debug.WriteLine("found object");
+
+            MistContainer container = new MistContainer();
+            container.Programs = new List<MistProgram>();
+
+            JObject obj = (JObject)token;
+            foreach (var prop in obj.Properties())
+            {
+                Debug.WriteLine($"{prop.Name}");
+                JToken statements = prop.Value;
+
+                List<Node> nodes = new List<Node>();
+
+                MistProgram mist = new MistProgram();
+                mist.Name = prop.Name.ToString();
+                mist.Statements = NodeList.Create(statements.Select(stmt => this.ToStatement((JObject)stmt)));
+
+                container.Programs.Add(mist);
+            }
+
+            return container;
+        }
+        else
+        {
+            Debug.WriteLine("sth else");
+            return null;
+        }
+    }
+
+    public Node? ToStatement(JObject obj)
+    {
+        if (!obj.ContainsKey("stmt_type"))
+        {
+            return null;
+        }
+
+        string stmt_type = obj["stmt_type"].ToString();
+        if (stmt_type == "Block")
+        {
+            JArray stmts = (JArray)obj.SelectToken("$.stmts", true);
+            NodeList<Statement> statements = NodeList.Create(stmts.Select(stmt => (Statement)this.ToStatement((JObject)stmt)));
+            return new BlockStatement(statements);
+        }
+        else if (stmt_type == "Expr")
+        {
+            return null;
+        }
+        else if (stmt_type == "Function")
+        {
+            Identifier ident = new Identifier(obj.SelectToken("$.name.value", true).ToString());
+            List<Node> parameters = new List<Node>();
+            BlockStatement body = (BlockStatement)this.ToStatement((JObject)obj.SelectToken("$.body", true));
+
+            //var ret = new FunctionDeclaration(ident, NodeList.Create(parameters), body, false, false, false);
+            var ret = new FunctionDeclaration(ident, NodeList.Create(parameters), new BlockStatement(NodeList.Create(new List<Statement>())), false, false, false);
+
+            string tmp = ret.ToJavaScriptString();
+
+            return ret;
+        }
+        else if (stmt_type == "Var")
+        {
+            Identifier ident = new Identifier(obj.SelectToken("$.name.value", true).ToString());
+            Expression expr = this.ToExpression((JObject)obj.SelectToken("$.initializer", true));
+
+            List<VariableDeclarator> declarators = new List<VariableDeclarator>();
+            declarators.Add(new VariableDeclarator(ident, expr));
+            return new VariableDeclaration(NodeList.Create(declarators), VariableDeclarationKind.Var);
+        }
+        else if (stmt_type == "Simultaneous")
+        {
+            return null;
+        }
+        else if (stmt_type == "Free")
+        {
+            return null;
+        }
+        else if (stmt_type == "If")
+        {
+            return null;
+        }
+        else if (stmt_type == "Return")
+        {
+            return null;
+        }
+        return null;
+    }
+
+    public Node? ToToken(JObject obj)
+    {
+        if (!obj.ContainsKey("token_type"))
+        {
+            return null;
+        }
+
+        string token_type = obj["token_type"].ToString();
+        if (token_type == "Identifier")
+        {
+            return null;
+        }
+        return null;
+    }
+
+    public Expression ToExpression(JObject obj)
+    {
+        if (!obj.ContainsKey("expr_type"))
+        {
+            return null;
+        }
+
+        string token_type = obj["expr_type"].ToString();
+        if (token_type == "Literal")
+        {
+            return null;
+        }
+        else if (token_type == "Named")
+        {
+            string ident = obj.SelectToken("$.name.value", true).ToString();
+            return new Identifier(ident);
+        }
+        else if (token_type == "Unary")
+        {
+            return null;
+        }
+        else if (token_type == "Binary")
+        {
+            return null;
+        }
+        else if (token_type == "Logical")
+        {
+            return null;
+        }
+        else if (token_type == "Assign")
+        {
+            return null;
+        }
+        else if (token_type == "Call")
+        {
+            Expression callee = this.ToExpression((JObject)obj.SelectToken("$.call", true));
+            JArray args = (JArray)obj.SelectToken("$.args", true);
+            List<Expression> args_list = args.Select(arg => this.ToExpression((JObject)arg)).ToList();
+            return new CallExpression(callee, NodeList.Create(args_list), false);
+        }
+        else if (token_type == "Grouping")
+        {
+            return null;
+        }
+        return null;
+    }
+
+    public override void WriteJson(Newtonsoft.Json.JsonWriter writer, MistContainer? value, JsonSerializer serializer)
+    {
+        throw new NotImplementedException();
+    }
+}
 
 public class MistDecompiler
 {
@@ -10,20 +202,11 @@ public class MistDecompiler
     {
         var text = File.ReadAllText(input);
 
-        var data = JObject.Parse(text);
-        
-        var functions = NodeList.Create(data.Properties().Select(Statement (p) =>
-        {
-            return new FunctionDeclaration(
-                new Identifier(p.Name.Replace(".mist", "")),
-                [],
-                new BlockStatement([]),
-                false, 
-                false, 
-                false
-            );
-        }));
-        
-        return new Script(functions, false).ToString();
+        var settings = new JsonSerializerSettings();
+        settings.Converters.Add(new MistContainerConverter());
+
+        MistContainer container = JsonConvert.DeserializeObject<MistContainer>(text, settings);
+
+        return container.Programs[0].ToJavaScriptString();
     }
 }
---
+++ ModsOfMistriaInstallerLibTests/Tools/DecompilerTest.cs
@@ -0,0 +1,77 @@
+﻿using Esprima.Utils;
+using Garethp.ModsOfMistriaInstallerLib.Tools.Decompiler;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ModsOfMistriaInstallerLibTests.Tools;
+
+[TestFixture]
+public class DecompilerTest
+{
+
+    [SetUp]
+    public void SetUp()
+    {
+    }
+
+    [Test]
+    public void ShouldDecompileEmptyFunction()
+    {
+        string ast = """
+{
+    "stmt_type": "Function",
+    "name": {"token_type": "Identifier", "value": "go_to_bed"},
+    "params": [],
+    "body": {
+        "stmt_type": "Block",
+        "stmts": []
+    },
+    "resolve": "null"
+}
+""";
+        string expected_js = "function go_to_bed(){}";
+
+        MistContainerConverter mistContainerConverter = new MistContainerConverter();
+        JObject obj = (JObject)JToken.Parse(ast);
+        string decompiled_js = mistContainerConverter.ToStatement(obj).ToJavaScriptString();
+
+        Assert.That(decompiled_js, Is.EqualTo(expected_js));
+    }
+
+    [Test]
+    [Ignore("The file day_zero.json was not committed. The expected_js is shorter than the actual result. Not all decompilations are implemented.")]
+    public void ShouldDecompileMistAst()
+    {
+        var decompiler = new MistDecompiler();
+        string decompiled_js = decompiler.Decompile("..\\..\\..\\..\\mists\\day_zero.json");
+
+        string expected_js = """
+function go_to_bed()
+{
+    var x = __get_new_day_spawn_x();
+    var y = __get_new_day_spawn_y();
+    camera_follow(ari);
+    simultaneous
+    {
+        walk(ari, x, y);
+        set_move_speed(ari, 0.5);
+    }
+    face(ari, south);
+    wait(1);
+    request_music_stop(1);
+    simultaneous
+    {
+        play_sound(""Music/Jingles/GoToSleep"");
+        {
+            animate(ari, blink);
+            wait(0.2);
+            freeze_ari();
+        }
+        fade_out(8);
+    }
+}
+""";
+        
+        Assert.That(decompiled_js, Is.EqualTo(expected_js));
+    }
+}
