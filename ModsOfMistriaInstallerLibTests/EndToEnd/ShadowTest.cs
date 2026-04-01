b84695988d06833a14592a2aeba294398c89307d
Gareth <blackfireweb@gmail.com>
2026-04-01 12:12:37 +0100

    Add end-to-end tests for shadows

---
+++ ModsOfMistriaInstallerLibTests/EndToEnd/ShadowTest.cs
@@ -0,0 +1,68 @@
+﻿using Garethp.ModsOfMistriaInstallerLib.Generator;
+using Garethp.ModsOfMistriaInstallerLib.Installer;
+using Garethp.ModsOfMistriaInstallerLib.Installer.UMT;
+using Garethp.ModsOfMistriaInstallerLib.ModTypes;
+using ModsOfMistriaInstallerLibTests.Fixtures;
+using ModsOfMistriaInstallerLibTests.Utils;
+using Newtonsoft.Json.Linq;
+
+namespace ModsOfMistriaInstallerLibTests.EndToEnd;
+
+[TestFixture]
+public class ShadowTest
+{
+    private IMod _mockMod;
+    private readonly MockFileModifier _fileModifier = new(new Dictionary<string, string>
+    {
+        { "shadow_manifest.json", "{}" }
+    });
+    
+    private readonly MockInstaller _installer = new([
+        new ShadowGenerator()
+    ], [
+        new ShadowManifestInstaller(),
+    ]);
+
+    [SetUp]
+    public void SetUp()
+    {
+        _mockMod = new MockMod(new Dictionary<string, string> {
+            { "shadows/shadow.json", new JObject {
+                { "test_shadow", new JObject {
+                    { "regular_sprite_name", "test_sprite" },
+                    { "sprite", "images/sprite.png" },
+                    { "is_animated", false }
+                }}
+            }.ToString() },
+            { "images/sprite.png", "" },
+        });
+    }
+
+    [Test]
+    public void ShouldAddSpriteImage()
+    {
+        var mod = _mockMod;
+        var generatedInformation = _installer.InstallMods([mod], _fileModifier);
+        
+        Assert.That(generatedInformation.Sprites, Contains.Key(mod.GetId()));
+        Assert.That(
+            generatedInformation.Sprites[mod.GetId()], 
+            Has.Some.Matches((SpriteData sprite) => 
+                sprite.Name == "test_shadow" &&
+                sprite.Location == "images/sprite.png" &&
+                !sprite.IsAnimated
+            )
+        );
+    }
+
+    [Test]
+    public void ShouldAddShadowManifest()
+    {
+        _installer.InstallMods([_mockMod], _fileModifier);
+        
+        Assert.That(_fileModifier.GetFile("shadow_manifest.json"), new MatchesJsonConstraint(new JObject
+        {
+            { "test_sprite", "test_shadow" }
+        }));
+    }
+}
---
+++ ModsOfMistriaInstallerLibTests/Models/ShadowSpriteTest.cs
@@ -0,0 +1,177 @@
+﻿using Garethp.ModsOfMistriaInstallerLib.Generator;
+using Garethp.ModsOfMistriaInstallerLib.Lang;
+using Garethp.ModsOfMistriaInstallerLib.Models;
+using Garethp.ModsOfMistriaInstallerLib.ModTypes;
+using ModsOfMistriaInstallerLibTests.Fixtures;
+
+namespace ModsOfMistriaInstallerLibTests.Models;
+
+[TestFixture]
+public class ShadowSpriteTest
+{
+    private IMod _mockMod;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _mockMod = new MockMod([
+            "images/sprite.png",
+            "images/animation/1.png",
+            "images/empty"
+        ]);
+    }
+
+    private static ShadowSprite GetMockShadowSprite()
+    {
+        var sprite = new ShadowSprite
+        {
+            RegularSpriteName = "test_sprite",
+            Sprite = "images/sprite.png",
+            IsAnimated = false
+        };
+
+        return sprite;
+    }
+
+    [Test]
+    public void ShouldHaveNoErrorsForValidShadowSpriteWithNoAnimation()
+    {
+        var shadowSprite = GetMockShadowSprite();
+        var validation = shadowSprite.Validate(new Validation(), _mockMod, "shadow.json", "shadow");
+
+        var expectedValidation = new Validation();
+
+        Assert.That(validation, Is.EqualTo(expectedValidation).Using(new ValidationComparer()));
+    }
+    
+    [Test]
+    public void ShouldHaveNoErrorsForValidShadowSpriteWithAnimation()
+    {
+        var shadowSprite = GetMockShadowSprite();
+        shadowSprite.Sprite = "images/animation";
+        shadowSprite.IsAnimated = true;
+        
+        var validation = shadowSprite.Validate(new Validation(), _mockMod, "shadow.json", "shadow");
+
+        var expectedValidation = new Validation();
+
+        Assert.That(validation, Is.EqualTo(expectedValidation).Using(new ValidationComparer()));
+    }
+
+    [Test]
+    public void ShouldHaveErrorForEmptyRegularSpriteName()
+    {
+        var shadowSprite = GetMockShadowSprite();
+        shadowSprite.RegularSpriteName = "";
+        var validation = shadowSprite.Validate(new Validation(), _mockMod, "shadow.json", "shadow");
+
+        var expectedValidation = new Validation();
+        expectedValidation.AddError(
+            _mockMod, 
+            "shadow.json",
+            string.Format(Resources.CoreErrorShadowHasNoSprite, "shadow")
+        );
+
+        Assert.That(validation, Is.EqualTo(expectedValidation).Using(new ValidationComparer()));
+    }
+    
+    [Test]
+    public void ShouldHaveErrorForEmptyNonAnimatedSprite()
+    {
+        var shadowSprite = GetMockShadowSprite();
+        shadowSprite.Sprite = "";
+        var validation = shadowSprite.Validate(new Validation(), _mockMod, "shadow.json", "shadow");
+
+        var expectedValidation = new Validation();
+        expectedValidation.AddError(
+            _mockMod,
+            "shadow.json",
+            string.Format(Resources.CoreErrorShadowHasNoLocation, "shadow")
+        );
+        
+        expectedValidation.AddError(
+            _mockMod, 
+            "shadow.json",
+            string.Format(Resources.CoreItemDoesNotHaveValue, "Shadow shadow")
+        );
+
+        Assert.That(validation, Is.EqualTo(expectedValidation).Using(new ValidationComparer()));
+    }
+    
+    [Test]
+    public void ShouldHaveErrorForMissingNonAnimatedSprite()
+    {
+        var shadowSprite = GetMockShadowSprite();
+        shadowSprite.Sprite = "images/not-found.png";
+        var validation = shadowSprite.Validate(new Validation(), _mockMod, "shadow.json", "shadow");
+
+        var expectedValidation = new Validation();
+        expectedValidation.AddError(
+            _mockMod, 
+            "shadow.json",
+            string.Format(Resources.CoreSpriteFileDoesNotExist, "Shadow shadow", "images/not-found.png")
+        );
+
+        Assert.That(validation, Is.EqualTo(expectedValidation).Using(new ValidationComparer()));
+    }
+    
+    [Test]
+    public void ShouldHaveErrorForEmptyAnimatedSprite()
+    {
+        var shadowSprite = GetMockShadowSprite();
+        shadowSprite.Sprite = "";
+        shadowSprite.IsAnimated = true;
+        var validation = shadowSprite.Validate(new Validation(), _mockMod, "shadow.json", "shadow");
+
+        var expectedValidation = new Validation();
+        expectedValidation.AddError(
+            _mockMod,
+            "shadow.json",
+            string.Format(Resources.CoreErrorShadowHasNoLocation, "shadow")
+        );
+        
+        expectedValidation.AddError(
+            _mockMod, 
+            "shadow.json",
+            string.Format(Resources.CoreItemDoesNotHaveValue, "Shadow shadow")
+        );
+
+        Assert.That(validation, Is.EqualTo(expectedValidation).Using(new ValidationComparer()));
+    }
+    
+    [Test]
+    public void ShouldHaveErrorForMissingAnimatedSprite()
+    {
+        var shadowSprite = GetMockShadowSprite();
+        shadowSprite.Sprite = "images/not-found";
+        shadowSprite.IsAnimated = true;
+        var validation = shadowSprite.Validate(new Validation(), _mockMod, "shadow.json", "shadow");
+
+        var expectedValidation = new Validation();
+        expectedValidation.AddError(
+            _mockMod, 
+            "shadow.json",
+            string.Format(Resources.CoreSpriteFolderDoesNotExist, "Shadow shadow", "images/not-found")
+        );
+
+        Assert.That(validation, Is.EqualTo(expectedValidation).Using(new ValidationComparer()));
+    }
+    
+    [Test]
+    public void ShouldHaveErrorForEmptyDirectoryAnimatedSprite()
+    {
+        var shadowSprite = GetMockShadowSprite();
+        shadowSprite.Sprite = "images/empty";
+        shadowSprite.IsAnimated = true;
+        var validation = shadowSprite.Validate(new Validation(), _mockMod, "shadow.json", "shadow");
+
+        var expectedValidation = new Validation();
+        expectedValidation.AddError(
+            _mockMod, 
+            "shadow.json",
+            string.Format(Resources.CoreSpriteFolderIsEmpty, "Shadow shadow", "images/empty")
+        );
+
+        Assert.That(validation, Is.EqualTo(expectedValidation).Using(new ValidationComparer()));
+    }
+}
