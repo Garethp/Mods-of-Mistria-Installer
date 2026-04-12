afa6ddc426f87e34a74c13b4602e427761d5b2f1
Gareth Parker <blackfireweb@gmail.com>
2026-04-12 12:49:58 +0100

    Fix issues where incorrect item/object JSONs will cause blank mod lists (#63)

---
+++ ModsOfMistriaInstallerLibTests/EndToEnd/NewItemsTest.cs
@@ -0,0 +1,113 @@
+﻿using Garethp.ModsOfMistriaInstallerLib.Generator;
+using Garethp.ModsOfMistriaInstallerLib.Installer;
+using Garethp.ModsOfMistriaInstallerLib.Lang;
+using ModsOfMistriaInstallerLibTests.Fixtures;
+using ModsOfMistriaInstallerLibTests.TestUtils;
+using Newtonsoft.Json.Linq;
+
+namespace ModsOfMistriaInstallerLibTests.EndToEnd;
+
+[TestFixture]
+public class NewItemsTest
+{
+    private MockFileModifier _fileModifier;
+
+    private readonly MockInstaller _installer = new([
+        new NewItemsGenerator(),
+    ], [
+        new FiddleInstaller()
+    ]);
+
+    [SetUp]
+    public void SetUp()
+    {
+        _fileModifier = new(new Dictionary<string, string>
+        {
+            { "__fiddle__.json", "{ }" }
+        });
+    }
+
+    [Test]
+    public void ShouldInstallValidItemToFiddle()
+    {
+        var mod = new MockMod((new Dictionary<string, string>
+        {
+            {
+                "items/test.json", new JObject
+                {
+                    {
+                        "new_item", new JObject
+                        {
+                            { "overwrites_other_mod", false },
+                            { "foo", "bar" }
+                        }
+                    },
+                }.ToString()
+            }
+        }));
+
+        var expected = new JObject
+        {
+            { "extras/objects", new JArray() },
+            { "extras", new JObject {
+                { "objects", new JArray() },
+                { "items", new JArray
+                {
+                    new JObject
+                    {
+                        { "name", "new_item" },
+                        { "data", new JObject
+                        {
+                            { "foo", "bar" }
+                        }}
+                    }
+                } }
+            } },
+            { "extras/items", new JArray
+            {
+                new JObject
+                {
+                    { "name", "new_item" },
+                    { "data", new JObject
+                    {
+                        { "foo", "bar" }
+                    }}
+                }
+            } }
+        };
+
+        _installer.ValidateMods([mod]);
+        
+        Assert.That(mod.GetValidation(), Is.EqualTo(new Validation()).Using(new ValidationComparer()));
+        
+        _installer.InstallMods([mod], _fileModifier);
+        
+        Assert.That(_fileModifier.GetFile("__fiddle__.json"), new MatchesJsonConstraint(expected));
+    }
+
+    [Test]
+    public void ShouldValidateOverwritesOtherModIsRequired()
+    {
+        var mod = new MockMod((new Dictionary<string, string>
+        {
+            {
+                "items/test.json", new JObject
+                {
+                    {
+                        "new_item", new JObject
+                        {
+                            { "foo", "bar" }
+                        }
+                    },
+                }.ToString()
+            }
+        }));
+
+        _installer.ValidateMods([mod]);
+
+        var expected = new Validation();
+        expected.AddError(mod, "items/test.json", string.Format(Resources.CoreErrorNewItemHasNoOverwritesOtherMod, "new_item"));
+        
+        Assert.That(mod.GetValidation(), Is.EqualTo(expected).Using(new ValidationComparer()));
+    }
+}
---
+++ ModsOfMistriaInstallerLibTests/EndToEnd/NewObjectsTest.cs
@@ -0,0 +1,149 @@
+﻿using Garethp.ModsOfMistriaInstallerLib.Generator;
+using Garethp.ModsOfMistriaInstallerLib.Installer;
+using Garethp.ModsOfMistriaInstallerLib.Lang;
+using ModsOfMistriaInstallerLibTests.Fixtures;
+using ModsOfMistriaInstallerLibTests.TestUtils;
+using Newtonsoft.Json.Linq;
+
+namespace ModsOfMistriaInstallerLibTests.EndToEnd;
+
+public class NewObjectsTest
+{
+     private MockFileModifier _fileModifier;
+
+    private readonly MockInstaller _installer = new([
+        new NewObjectsGenerator(),
+    ], [
+        new FiddleInstaller()
+    ]);
+
+    [SetUp]
+    public void SetUp()
+    {
+        _fileModifier = new(new Dictionary<string, string>
+        {
+            { "__fiddle__.json", "{ }" }
+        });
+    }
+
+    [Test]
+    public void ShouldInstallValidItemToFiddle()
+    {
+        var mod = new MockMod(new Dictionary<string, string>
+        {
+            {
+                "objects/test.json", new JObject
+                {
+                    {
+                        "new_object", new JObject
+                        {
+                            { "category", "building" },
+                            { "overwrites_other_mod", false },
+                            { "data", new JObject
+                            {
+                                { "foo", "bar" }
+                            } }
+                        }
+                    },
+                }.ToString()
+            }
+        });
+
+        var expected = new JObject
+        {
+            { "extras/items", new JArray() },
+            { "extras", new JObject {
+                { "items", new JArray() },
+                { "objects", new JArray
+                {
+                    new JObject
+                    {
+                        { "name", "new_object" },
+                        { "category", "building" },
+                        { "data", new JObject
+                        {
+                            { "foo", "bar" }
+                        }}
+                    }
+                } }
+            } },
+            { "extras/objects", new JArray
+            {
+                new JObject
+                {
+                    { "name", "new_object" },
+                    { "category", "building" },
+                    { "data", new JObject
+                    {
+                        { "foo", "bar" }
+                    }}
+                }
+            } }
+        };
+
+        _installer.ValidateMods([mod]);
+        
+        Assert.That(mod.GetValidation(), Is.EqualTo(new Validation()).Using(new ValidationComparer()));
+        
+        _installer.InstallMods([mod], _fileModifier);
+        
+        Assert.That(_fileModifier.GetFile("__fiddle__.json"), new MatchesJsonConstraint(expected));
+    }
+
+    [Test]
+    public void ShouldValidateOverwritesOtherModIsRequired()
+    {
+        var mod = new MockMod(new Dictionary<string, string>
+        {
+            {
+                "objects/test.json", new JObject
+                {
+                    {
+                        "new_object", new JObject
+                        {
+                            { "category", "building" },
+                            { "data", new JObject
+                            {
+                                { "foo", "bar" }
+                            } }
+                        }
+                    },
+                }.ToString()
+            }
+        });
+
+        _installer.ValidateMods([mod]);
+
+        var expected = new Validation();
+        expected.AddError(mod, "objects/test.json", string.Format(Resources.CoreErrorNewObjectHasNoOverwritesOtherMod, "new_object"));
+        
+        Assert.That(mod.GetValidation(), Is.EqualTo(expected).Using(new ValidationComparer()));
+    }
+    
+    [Test]
+    public void ShouldValidateDataIsRequired()
+    {
+        var mod = new MockMod(new Dictionary<string, string>
+        {
+            {
+                "objects/test.json", new JObject
+                {
+                    {
+                        "new_object", new JObject
+                        {
+                            { "category", "building" },
+                            { "overwrites_other_mod", false }
+                        }
+                    },
+                }.ToString()
+            }
+        });
+
+        _installer.ValidateMods([mod]);
+
+        var expected = new Validation();
+        expected.AddError(mod, "objects/test.json", string.Format(Resources.CoreErrorNewObjectNoData, "new_object"));
+        
+        Assert.That(mod.GetValidation(), Is.EqualTo(expected).Using(new ValidationComparer()));
+    }
+}
