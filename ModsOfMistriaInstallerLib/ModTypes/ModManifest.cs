f0737f55247f7dc2229af1a604a776e8f469a408
Gareth <blackfireweb@gmail.com>
2026-07-04 18:24:13 +0100

    Add requirements parsing into Toml Manifest

--- ModsOfMistriaInstallerLib/ModTypes/ModManifest.cs
+++ ModsOfMistriaInstallerLib/ModTypes/ModManifest.cs
@@ -56,24 +56,42 @@ public class ModManifest
 
     public static ModManifest FromToml(TomlTable toml)
     {
-        // @TODO: Implement mod requirements
-        
         toml.TryGetValue("name", out var name);
         toml.TryGetValue("author", out var author);
         toml.TryGetValue("version", out var version);
         toml.TryGetValue("minInstallerVersion", out var minInstallerVersion);
         toml.TryGetValue("manifestVersion", out var manifestVersion);
-        toml.TryGetValue("requirements", out var requirements);
+        toml.TryGetValue("requirements", out var requirementsObject);
         toml.TryGetValue("download_url", out var downloadUrl);
         toml.TryGetValue("update_url", out var updateUrl);
+
+        List<ModRequirement> requirements = [];
+        IList<TomlTable> requirementsList = new List<TomlTable>();
+
+        if (requirementsObject is IList<object?> requirementsListUnknown)
+        {
+            requirementsList = requirementsListUnknown.OfType<TomlTable>().ToList();
+        } else if (requirementsObject is IList<TomlTable> requirementsListKnown)
+        {
+            requirementsList = requirementsListKnown;
+        }
         
+        requirements.AddRange(requirementsList.Select(requirement =>
+        {
+            requirement.TryGetValue("name", out var name);
+            requirement.TryGetValue("author", out var author);
+            requirement.TryGetValue("download_url", out var downloadUrl);
+            
+            return new ModRequirement(name?.ToString() ?? "", author?.ToString() ?? "", downloadUrl?.ToString());
+        }));
+
         return new ModManifest(
             name?.ToString() ?? "",
             author?.ToString() ?? "",
             version?.ToString() ?? "",
             minInstallerVersion?.ToString() ?? "0.1",
             manifestVersion?.ToString() ?? "1",
-            new List<ModRequirement>(),
+            requirements,
             downloadUrl?.ToString(),
             updateUrl?.ToString()
         );
