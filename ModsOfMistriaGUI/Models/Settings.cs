8ce621c72e516a7f139f16d6083ad929cc7cd8f2
Gareth <blackfireweb@gmail.com>
2026-08-02 13:02:42 +0100

    Add an error message for users trying to use MOMI on older versions of FoM

--- ModsOfMistriaGUI/Models/Settings.cs
+++ ModsOfMistriaGUI/Models/Settings.cs
@@ -2,7 +2,7 @@
 
 namespace Garethp.ModsOfMistriaGUI.Models;
 
-public partial class Settings: ObservableObject
+public partial class Settings : ObservableObject
 {
     public Settings()
     {
@@ -13,14 +13,21 @@ public partial class Settings: ObservableObject
         MistriaLocation = mistriaLocation ?? "";
         ModsLocation = modsLocation ?? "";
     }
-    
+
     [ObservableProperty] private string _mistriaLocation = "";
-    
+
     [ObservableProperty] private string _modsLocation = "";
-    
-    public bool ValidMistriaLocation() => !string.IsNullOrEmpty(MistriaLocation) && 
-                                          Directory.Exists(MistriaLocation) && (File.Exists(Path.Combine(MistriaLocation, "assets.zip")) || Directory.Exists(Path.Combine(MistriaLocation, "assets")));
-    
+
+    public bool ValidMistriaLocation() => !string.IsNullOrEmpty(MistriaLocation) &&
+                                          Directory.Exists(MistriaLocation) &&
+                                          (File.Exists(Path.Combine(MistriaLocation, "assets.zip")) ||
+                                           Directory.Exists(Path.Combine(MistriaLocation, "assets")));
+
     public bool ValidModsLocation() => !string.IsNullOrEmpty(ModsLocation) &&
                                        Directory.Exists(ModsLocation);
+
+    public bool WrongMistriaVersion() => !string.IsNullOrEmpty(MistriaLocation) && Directory.Exists(MistriaLocation) &&
+                                         (File.Exists(Path.Combine(MistriaLocation, "FieldsOfMistria.exe")) ||
+                                          File.Exists(Path.Combine(MistriaLocation, "FieldsOfMistria"))) &&
+                                         !File.Exists(Path.Combine(MistriaLocation, "assets.zip"));
 }
